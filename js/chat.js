9daefe2c091f2324e07232f81bb9cd2e65f73d0a
Muntazir2009 <102378365+Muntazir2009@users.noreply.github.com>
2026-04-08 20:07:51 +0530

    Update chat.js

--- js/chat.js
+++ js/chat.js
@@ -1,12 +1,18 @@
 // Firebase configuration - REPLACE WITH YOUR OWN CONFIG
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  databaseURL: "https://YOUR_PROJECT-default-rtdb.firebaseio.com",
-  projectId: "YOUR_PROJECT",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyBvsLNXMGsr-XQF-GE-EET1YOnICSMicOA",
+  authDomain: "bioquiz-chat.firebaseapp.com",
+  databaseURL: "https://bioquiz-chat-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "bioquiz-chat",
+  storageBucket: "bioquiz-chat.firebasestorage.app",
+  messagingSenderId: "616382882153",
+  appId: "1:616382882153:web:9c8a32401be847468d1df8"
 };
 
 // Initialize Firebase
